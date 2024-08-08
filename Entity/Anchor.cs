88ee8913c0f57f25353b6fedf80b1b2f77f3e135
ff <frend.fernando@LIVE.COM>
2024-08-08 08:33:08 -0700

    up html arrangement

---
+++ Entity/Anchor.cs
@@ -0,0 +1,11 @@
+﻿namespace BoshaTuft.Entity;
+
+public sealed class Anchor
+{
+    private string Id { get; set; } = Guid.NewGuid().ToString();
+    public required string Description { get; set; }
+    public required string LinkTo { get; set; }
+    public string Pic { get; set; } = "";
+    public string Alt { get; set; } = "";
+    public string DatedAt { get; set; } = DateTime.UtcNow.ToString("s");
+}
